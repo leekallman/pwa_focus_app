f027ca9da7b0dfbbef0a86f805ec010f90326323
Lisa Lee <lisaleekallman@gmail.com>
2021-04-22 17:30:43 +0200

    created a custom hook, useTimer

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import SettingsBtn from './components/settingsBtn.js';
 import SettingsModal from './components/settingsModal.js';
 import TimeOutModal from './components/timeOutModal.js';
 import './App.css';
-import timeOutAlarm from './assets/bell.mp3'
-import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
-const alarm = new Audio(timeOutAlarm)
-alarm.volume = 0.6
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+import useTimer from './hooks/useTimer.js'
+
 
 const AppContainer = styled.div`
 width:100vw;
@@ -70,8 +69,6 @@ const TimerWrapper = styled.div`
     background: none;
     color:#000;
   }
-
-
   `
 const Button = styled.button`
   align-items: center;
@@ -95,38 +92,11 @@ function formatTime(sec) {
 }
 
 function App() {
-  // initial Session time
-  let focusSessionDuration = 1
-  let breakSessionDuration = 5
-  let longBreakSessionDuration = 15
+  const {timer, toggleTimer} = useTimer();
 
-  // useState
-  const [focusInput, setFocusInput] = useState(focusSessionDuration);
-  const [breakInput, setBreakInput] = useState(breakSessionDuration);
-  const [longBreakInput, setLongBreakInput] = useState(longBreakSessionDuration);
-
-  const [focus, setFocus] = useState(true);
-  const [time, setTime] = useState(focusInput * 60);
-  const [isActive, setIsActive] = useState(false);
-  const [key, setKey] = useState(0);
-
+  // const [isActive, setIsActive] = useState(false);
   const [modal, setModal] = useState(false);
   const [timeOutModal, setTimeOutModal] = useState(false);
-
-
-  // functions
-  const handleChange = (e) => {
-    setFocusInput(e.target.value);
-    setTime(e.target.value * 60);
-    setKey(e.target.value)
-  }
-  const handleBreak = (e) => {
-    setBreakInput(e.target.value);
-  }
-
-  const handleLongBreak = (e) => {
-    setLongBreakInput(e.target.value);
-  }
 
   // modals
   const modalToggler = () => {
@@ -146,109 +116,84 @@ function App() {
     }
   }
 
-  // buttons
-  function start() {
-    setIsActive(!isActive);
-  }
-
-  function reset() {
-    setIsActive(false);
-    setFocus(true);
-    setTime(focusInput*60);
-    setKey(focusInput*60);
-  }
-
   useEffect(() => {
     let interval = null;
     // COUNTDOWN
-    if (isActive && time > 0) {
+    if (timer.isActive && timer.setTime > 0) {
       interval = setInterval(() => {
-        setTime(time => time - 1);
-      }, 1000);
+        toggleTimer('COUNTDOWN')
+      }, 10);
 
     // PAUSE
-    } else if (!isActive && time > 0) {
+    } else if (!timer.isActive && timer.time > 0) {
+      toggleTimer('BREAK')
       clearInterval(interval);
     }
     // TIMEOUT FOCUS SESSION
-    else if (time === 0 && focus === true) {
-      setIsActive(false);
+    else if (timer.time === 0 && timer.focus === true) {
+      toggleTimer('TIMEFORBREAK')
       clearInterval(interval);
       setTimeOutModal(true)
-      setFocus(!focus)
       setModal(false);
-      alarm.play();
-      setTime(breakInput * 60)
-      setKey(breakInput * 60);
-    }
-    // TIMEOUT BREAK SESSION
-    else if (time === 0 && focus === false) {
-      clearInterval(interval);
-      setIsActive(false);
-      setFocus(true);
-      alarm.play()
-      setTime(focusInput * 60);
-      setKey(focusInput * 60);
-    }
+      }
+      // alarm.play();
 
-    return () => clearInterval(interval);
-  }, [isActive, time, setTime, breakInput, focus, focusInput]);
+    // TIMEOUT BREAK SESSION
+    else if (timer.time === 0 && timer.focus === false) {
+      toggleTimer('TIMEFORRESET')
+      clearInterval(interval);
+      }
+      // alarm.play()
+
+    return () => clearInterval(interval)
+  }, [timer.isActive, timer.time, timer.setTime, timer.focus, toggleTimer]);
 
 
   return (
-    <AppContainer id="App" className={focus ? "focus" : "break"}>
+    <AppContainer id="App" className={timer.focus ? "focus" : "break"}>
       <GlobalStyles />
       <TimerWrapper id="timerWrapper" onClick={closeModal}>
-        <div className="timeLabel">{focus ? "Focus Time" : "Break Time"}</div>
+        <div className="timeLabel">{timer.focus ? "Focus Time" : "Break Time"}</div>
         <div className="timer-wrapper">
           <CountdownCircleTimer
-            isPlaying={isActive ? true : false}
-            duration={time}
+            isPlaying={timer.isActive ? true : false}
+            duration={timer.time}
             colors={"#000"}
             strokeWidth={3}
             strokeLinecap={"square"}
             size={250}
             renderAriaTime
-            key={key}
+            key={timer.key}
           >
-            <div className={`timer ${isActive ? "black" : "none"}`}>
-              <div className="value">{formatTime(time)}</div>
+            <div className={`timer ${timer.isActive ? "black" : "none"}`}>
+              <div className="value">{formatTime(timer.time)}</div>
             </div>
           </CountdownCircleTimer>
         </div>
         <div className="actions">
-          <Button className={`button ${isActive ? "pause" : "start"}`} onClick={start}>
-            {isActive ? 'Pause' : 'Start'}
+          <Button className={`button ${timer.isActive ? "pause" : "start"}`} onClick={toggleTimer('START')}>
+            {timer.isActive ? 'Pause' : 'Start'}
           </Button>
 
-          <Button className="button reset" onClick={reset}>Reset</Button>
+          <Button className="button reset" onClick={toggleTimer('RESET')}>Reset</Button>
         </div>
       </TimerWrapper>
 
       <SettingsBtn modalToggler={modalToggler} />
       <SettingsModal
+        timer={timer}
         modal={modal}
-        focusInput={focusInput}
-        breakInput={breakInput}
-        longBreakInput={longBreakInput}
-        handleChange={handleChange}
-        handleBreak={handleBreak}
-        handleLongBreak={handleLongBreak}
+        handleChange={toggleTimer('CHANGEFOCUS')}
+        handleBreak={toggleTimer('CHANGEBREAK')}
+        handleLongBreak={toggleTimer('CHANGELONGBREAK')}
       />
-      <TimeOutModal
+      {/* <TimeOutModal
+        timer={timer}
+        start={toggleTimer('START')}
         timeOutModal={timeOutModal}
         timeOutToggler={timeOutToggler}
-        setTime={setTime}
-        breakInput={breakInput}
-        longBreakInput={longBreakInput}
-        isActive={isActive}
-        setIsActive={setIsActive}
-        focus={focus}
-        setFocus={setFocus}
-        focusInput={focusInput}
-        setFocusInput={setFocusInput}
-        setKey={setKey}
-      />
+        isActive={timer.isActive}
+      /> */}
     </AppContainer >
   );
 }
